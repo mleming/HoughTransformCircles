c633cefc3681e0e7e14988f43b4d33a7a6c67109
mleming <matthew.j.leming@gmail.com>
2014-09-18 08:53:28 -0400

    Update HW1_run.m

--- HW1_run.m
+++ HW1_run.m
@@ -9,16 +9,21 @@
 % explain it step by step.
 % The imDeriv functions were made by Nathan Roach.
 
-radius = 50;
-intensity_polarity = 0; % 0 for white circles, 1 for black circles
+radius = 20;
+number_of_circles = 10;
+circle_blurring_sigma = 10;
+noise_intensity = 1;
+circle_opacity_range = [0.1 1];
+width = 500;
+height = 500;
+intensity_polarity = 1; % 0 for white circles, 1 for black circles
 sigmoid_mean = 0.4;
 sigmoid_standard = 0.1;
-parzen_standard_deviation = 15; %Blurring radius
+parzen_standard_deviation = 15; %Blurring radius, in pixels
 gradient_magnitude_threshold = sigmoid_mean - 3*sigmoid_standard;
 
 
-[image, actual_coords] = image_generator(radius, [0.3 1], 10, 1, 0.5, 500, 500, intensity_polarity);
-   
+[image, actual_coords] = image_generator(radius, circle_opacity_range, number_of_circles, circle_blurring_sigma, noise_intensity, width, height, intensity_polarity);
 if size(image, 3) == 3
     image = rgb2gray(image);
 end
